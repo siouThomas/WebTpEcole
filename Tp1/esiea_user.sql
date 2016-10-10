01ea0fa548483a0968bbc489724b69001edd3fe3
Haypod <liebert@et.esiea.fr>
2016-10-10 14:44:03 +0200

    Add files via upload

--- Tp1/esiea_user.sql
+++ Tp1/esiea_user.sql
@@ -1,20 +1,4 @@
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Base de données: `test`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `esiea_user`
---
 
 CREATE TABLE IF NOT EXISTS `esiea_user` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -27,14 +11,8 @@ CREATE TABLE IF NOT EXISTS `esiea_user` (
   KEY `ID` (`ID`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=51 ;
 
---
--- Contenu de la table `esiea_user`
---
 
 INSERT INTO `esiea_user` (`ID`, `name`, `firstname`, `city`, `old`, `section`, `other`) VALUES
-(1, 'Gorand', 'Thomas', 'Paris', 23, 5, 'des info en plus blabla '),
-(2, 'Gilles', 'Rivière', 'Paris', 29, 5, 'des info en plus blabla '),
-(3, 'Antoine', 'Liebert', 'Paris', 43, 5, 'CentOS-7.2'),
-(4, 'Duong', 'Elhy', 'Paris', 230, 5, 'des info en plus blabla '),
-(5, 'George', 'Alain', 'Paris', 2, 5, 'autre')
+(1, 'GORAND', 'Thomas', 'Paris', 23, 5, 'des info'),
+(2, 'LIEBERT', 'Gilles', 'Paris', 24, 6, 'des info encore'),
 ;
